f32f136c601ece1e5d04430178d4311f7fbc373c
jrwatson94 <j.watsonreid@gmail.com>
2020-10-27 20:58:06 -0400

    core deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log('hello world')
+
+
+  document.querySelector('input[type=submit]').addEventListener("click", function(event){
+    let taskInput = document.querySelector('#new-task-description').value
+
+    let list = document.querySelector('#tasks')
+    let task = document.createElement('li')
+    task.innerText = taskInput
+    list.appendChild(task)
+
+    event.preventDefault();
+  })
+
+
 });
